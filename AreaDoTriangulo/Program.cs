feeac59a24dc427c16ce9b63606e0b93addff3f2
Deivison <deivisoneditordevideos@gmail.com>
2022-06-24 18:50:56 -0300

    Starting

---
+++ AreaDoTriangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaDoTriangulo
+{
+    class Area_Do_Triangulo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do lado A:");
+            double ladoA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor do lado B:");
+            double ladoB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor do lado C:");
+            double ladoC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            var triangulo = new Triangulo(ladoA, ladoB, ladoC);
+
+            Console.WriteLine($"Área do triângulo: {triangulo.CalcularArea().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ AreaDoTriangulo/Triangulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaDoTriangulo
+{
+    public class Triangulo
+    {
+        public Triangulo(double ladoA, double ladoB, double ladoC)
+        {
+            LadoA = ladoA;
+            LadoB = ladoB;
+            LadoC = ladoC;
+        }
+
+        public double LadoA { get; set; }
+        public double LadoB { get; set; }
+        public double LadoC { get; set; }
+
+        public double CalcularArea()
+        {
+            double area = 0, p = 0;
+
+            p = (LadoA + LadoB + LadoC) / 2;
+            area = Math.Sqrt(p * (p - LadoA) * (p - LadoB) * (p - LadoC));
+
+            return area;
+        }
+    }
+}
